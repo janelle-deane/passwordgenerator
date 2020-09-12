3d24050348d7b5d8edb249b64efa50de724a6b2a
JD <janelle.m.deane@gmail.com>
2020-09-11 18:16:20 -0700

    final debugging og JavaScript

--- script.js
+++ script.js
@@ -10,16 +10,15 @@ function writePassword() {
 
 }
 
-// To Do: 
 // Create generatePassword function and returns all of my code should live inside this code
-
 function generatePassword(){
+  
 // Create arrays with all needed characters (lowercase, lowercase, numeric and special)
 var alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var alphaLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var numeric =["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]; 
 var specialCharacter = ["\'", "\"", "#", "$", "%", "&", "!", "(", ")", "*", "+", "\&", "{", "'", "|", "}", "~", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^"];
-// create a new var that holds all of the chosen characters
+// create a new var that holds all of the chosen characters to be selected from
 var chosenArray =[]; 
 // create a new array with to hold final password 
 var passwordArray=[]; 
@@ -30,40 +29,39 @@ charNumeric = false;
 charUpper = false; 
 charLower = false; 
 
-
 // Create a series of prompts with conditionals:
 // 1. var= Prompt-length of password (validation: 8-128 characters) **Is there a way to loop back to orginal prompt if condition is not met?
 var charLength = prompt ("How many characters do you want you password? Please enter a number between 8-128: ");
-console.log (charLength)
+console.log (charLength);
 if (charLength >= 8 && charLength <=129){
 
         // 2.Confirm-Special Characters?
-        charSpecial =confirm ("Do you want special characters?")
-        console.log(charSpecial)
+        charSpecial =confirm ("Do you want special characters?");
+        console.log(charSpecial);
  
 
         // 3.Confirm-Numeric Characters?
-        charNumeric =confirm ("Do you want numbers?")
-        console.log(charNumeric)
+        charNumeric =confirm ("Do you want numbers?");
+        console.log(charNumeric);
 
         // 4.Confirm-UpperCase Characters?
-        charUpper =confirm ("Do you want UPPER CASE letters?")
-        console.log(charUpper)
+        charUpper =confirm ("Do you want UPPER CASE letters?");
+        console.log(charUpper);
 
         // 5.Confirm-LowerCharacters?
-        charLower =confirm ("Do you want lower case letters?")
-        console.log(charLower)
+        charLower =confirm ("Do you want lower case letters?");
+        console.log(charLower);
 
 // Add validations to make sure at least one type character is selected and alert the user that they need to chose 
 
 // add validation to after logic       
         while(charLower===false && charNumeric===false && charSpecial===false && charUpper===false) {
-          alert("Please choose one Character trait to be included in your password")
-          charSpecial =confirm ("Do you want special characters?")
-          charNumeric =confirm ("Do you want numbers?")
-          charUpper =confirm ("Do you want UPPER CASE letters?")
-          charLower =confirm ("Do you want lower case letters?")
-        }
+          alert("Please choose one Character trait to be included in your password");
+          charSpecial =confirm ("Do you want special characters?");
+          charNumeric =confirm ("Do you want numbers?");
+          charUpper =confirm ("Do you want UPPER CASE letters?");
+          charLower =confirm ("Do you want lower case letters?");
+        };
 
         if (charSpecial===true){
           chosenArray.push(...specialCharacter); 
@@ -82,15 +80,10 @@ if (charLength >= 8 && charLength <=129){
         }; 
 
         // Check combination Array & array length
-        console.log(chosenArray);
-        
+        console.log(chosenArray);  
 
-        // create an else statement requiring one character statement--having issues
-        // else {
-        //   
-
-        // Add loop that encompasses arrays and is dedicated by first prompt 
-        for (var i =0; i<chosenArray.length; i++){
+        // Add loop that encompasses arrays and is decided by first prompt 
+        for (var i =0; i<charLength; i++){
         var index = Math.floor(Math.random()*chosenArray.length);
         var part = chosenArray[index];
         console.log(part);
